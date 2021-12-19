22b5947b9a222d5bd0687d4f9b0765ff72b20ba8
ishitarajoria <96059659+ishitarajoria@users.noreply.github.com>
2021-12-19 21:57:51 +0530

    Add files via upload

---
+++ occurence of digits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+int a[10]={0},num,rem,i;
+printf("enter the no");
+scanf("%d",&num);
+while(num)
+{
+    rem=num%10;
+    a[rem]=a[rem]+1;
+    num=num/10;
+}
+
+printf("\n");
+
+for(i=0;i<10;i++)
+{
+
+   if(a[i]!=0)
+     print("%d" has appeared %d times\n",i,a[i]);
+}
+return 0;
+}
